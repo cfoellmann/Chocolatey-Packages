af5938f7796d466c9dcf911fe406ecf34e2bb223
Christian Foellmann <foellmann@foe-services.de>
2015-07-27 11:57:31 +0200

    no install UI for installer

--- manual/renamer.install/tools/chocolateyInstall.ps1
+++ manual/renamer.install/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@ $packageName = 'renamer.install'
 $installerType = 'EXE'
 $url = 'https://dl.dropboxusercontent.com/u/22096967/renamer-6.2.exe'
 
-$silentArgs = '/silent' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
+$silentArgs = '/VERYSILENT'
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
