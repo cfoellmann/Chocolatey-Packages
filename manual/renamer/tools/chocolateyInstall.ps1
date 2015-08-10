bf07ca364be724e3163c7e2232330274c60ec33d
Christian Foellmann <foellmann@foe-services.de>
2015-08-10 07:14:35 +0200

    make renamer 'meta package' again

--- manual/renamer/tools/chocolateyInstall.ps1
+++ manual/renamer/tools/chocolateyInstall.ps1
@@ -1,8 +1,1 @@
-$packageName = 'renamer.install'
-$installerType = 'EXE'
-$url = 'http://www.den4b.com/?x=get&product=renamer&type=installer&version=6.2&token=chocolatey'
-
-$silentArgs = '/VERYSILENT'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+#Install-VirtualPackage 'renamer.portable' 'renamer.install'
