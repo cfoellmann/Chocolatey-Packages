afa13aef84d0dda2985a9d66df4a4d4f2789d3c4
Doug Hubbard <doug@dougis.com>
2018-03-09 18:10:50 -0800

    Updated for Vagrant manager 1.0.0.6

--- manual/vagrant-manager/tools/chocolateyInstall.ps1
+++ manual/vagrant-manager/tools/chocolateyInstall.ps1
@@ -1,7 +1,11 @@
 $packageName = 'vagrant-manager'
 $installerType = 'EXE'
-$url = 'https://github.com/lanayotech/vagrant-manager-windows/releases/download/1.0.0.3/vagrant-manager-windows-1.0.0.3.exe' # download url
+$url = 'https://github.com/lanayotech/vagrant-manager-windows/releases/download/1.0.0.6/vagrant-manager-windows-1.0.0.6.zip' # download url
+$zipFile = 'vagrant-manager-windows-1.0.0.6.zip' # name of the actual EXE we extract
+$exeFile = 'vagrant-manager-windows-1.0.0.6.exe' # name of the actual EXE we extract
 $silentArgs = '/verysilent /norestart'
 $validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath "$toolsDir\$zipFile" -Url $url
+Get-ChocolateyUnzip -PackageName $packageName  -FileFullPath "$toolsDir\$zipFile" -Destination $env:TEMP
+Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" "$env:TEMP\$exeFile"   -validExitCodes $validExitCodes
